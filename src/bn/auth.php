8b3091f14bdd4760545443e382391faf63ab492c
Abu Shahriar Ratul <ratulcse10@gmail.com>
2016-06-16 21:35:36 +0600

    [bn] New language initiated

---
+++ src/bn/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'লগিনের জন্য প্রদত্ত তথ্যসমূহের কোন মিল আমাদের রেকর্ডে নেই',
+    'throttle' => 'লগিজের জন্য অনেকবার চেষ্টা করা হয়েছে, অনুগ্রহ করে :seconds সেকেন্ড পরে আবার চেষ্টা করুন',
+
+];
---
+++ src/bn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'পাসওয়ার্ড অবশ্যই কমপক্ষে ৬ অক্ষরের হতে হবে এবং কনফার্ম পাসওয়ার্ডের সাথে মিলতে হবে',
+    'reset'    => 'আপনার পাসওয়ার্ড রিসেট করা হয়েছে !',
+    'sent'     => 'আমরা আপনাকে পাসওয়ার্ড রিসেট করার লিংক ইমেইল করেছি !',
+    'token'    => 'পাসওয়ার্ড রিসেট করার এই টোকেনটি সঠিক নয়',
+    'user'     => "আমরা প্রদত্ত ইমেইল ঠিকানায় কোন ব্যবহারকারী সনাক্ত করতে পারছি না",
+
+];
